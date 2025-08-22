9370dc22e6671e3a74130a1f1266ae6f97df9227
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-22 13:25:52 +0900

    feat: problem pk primary 처리

--- apps/backend/src/problem/schema/problem.schema.ts
+++ apps/backend/src/problem/schema/problem.schema.ts
@@ -9,7 +9,7 @@ export const siteEnum = pgEnum(
 );
 
 export const problem = pgTable('problem', {
-  id: serial('id'),
+  id: serial('id').primaryKey(),
   problemUrl: text('problemUrl').notNull().unique(),
   site: siteEnum('site').notNull(),
   memberId: integer('member_id'),
