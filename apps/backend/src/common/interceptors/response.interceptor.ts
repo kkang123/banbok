25cec26ec86c493055c10c75088bf5f8ab99b05d
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-24 20:59:35 +0900

    feat: 공통 응답구조 인터셉터로 구현

---
+++ apps/backend/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,44 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    const parseMetadatas = function (req: Request, res: Response) {
+      const statusCode = res.statusCode;
+
+      const user = req?.user;
+      const ip = req?.ip;
+      const method = req?.method;
+      const originalUrl = req?.originalUrl;
+
+      return {
+        statusCode,
+        user,
+        ip,
+        method,
+        originalUrl,
+      }
+    }
+
+    const transformResponse = (data: unknown) => {
+      const req = context.switchToHttp().getRequest<Request>();
+      const res = context.switchToHttp().getResponse<Response>();
+
+      const { statusCode } = parseMetadatas(
+        req,
+        res,
+      );
+
+      return {
+        statusCode,
+        data,
+      };
+    }
+
+    return next.handle().pipe(
+      map(transformResponse),
+    )
+  }
+}
