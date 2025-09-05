f0e3cb41ce0f588182e082ab95c91aeb70196dc0
HoyeongJeon <hoyeong0302@gmail.com>
2025-09-05 21:45:30 +0900

    feat: Cookie에서 jwt 토큰 추출하도록 수정

--- apps/backend/src/auth/strategies/jwt.strategy.ts
+++ apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.accessToken;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerConfig } from './common/configurations';
 import { CorsConfig } from './common/configurations/cors.config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.enableCors(CorsConfig.getCorsOptions());
   SwaggerConfig.setUp(app)
   await app.listen(3001);
