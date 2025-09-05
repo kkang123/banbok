8ad76552e2798ef18c6bf02ce34d39df18ad6d1d
HoyeongJeon <hoyeong0302@gmail.com>
2025-09-05 22:39:09 +0900

    feat: 문제제출 타입 수정

--- apps/backend/src/problem/dto/request/submit-problem.request.ts
+++ apps/backend/src/problem/dto/request/submit-problem.request.ts
@@ -2,9 +2,21 @@ import { SubmitProblemRequestDto as ISubmitProblemDto } from '@banbok/shared';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SubmitProblemRequestDto implements ISubmitProblemDto {
-    @ApiProperty({
-        example: 'https://www.acmicpc.net/problem/1000',
-        description: '문제 링크',
-    })
-    readonly link: string;
+  @ApiProperty({
+    example: "A+B",
+    description: '문제 이름',
+  })
+  readonly title: string;
+
+  @ApiProperty({
+    example: 'BOJ',
+    description: '문제 사이트 이름',
+  })
+  readonly site: string;
+
+  @ApiProperty({
+    example: 'https://www.acmicpc.net/problem/1000',
+    description: '문제 링크',
+  })
+  readonly link: string;
 }
