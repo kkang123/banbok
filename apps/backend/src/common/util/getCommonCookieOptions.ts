44146ca1ef184f8913ddceb1f5a882d318d1f670
HoyeongJeon <hoyeong0302@gmail.com>
2025-09-10 11:43:00 +0900

    feat: 쿠키 도메인 프론트엔드 도메인으로 설정

--- apps/backend/src/common/util/getCommonCookieOptions.ts
+++ apps/backend/src/common/util/getCommonCookieOptions.ts
@@ -2,10 +2,13 @@ import { COOKIE_CONFIG } from '../constants';
 import { ConfigService } from '@nestjs/config';
 
 export const getCommonCookieOptions = (configService: ConfigService) => {
+  const isProduction = configService.get<string>('NODE_ENV') === 'production';
+  
   return {
     httpOnly: true,
-    secure: configService.get<string>('NODE_ENV') === 'production',
+    secure: isProduction,
     sameSite: COOKIE_CONFIG.SAME_SITE,
     path: '/',
+    domain: isProduction ? 'banbok-coding.vercel.app' : 'localhost',
   };
 };
