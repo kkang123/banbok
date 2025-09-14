79b2b6b0a84348cacf80ba673e56dc1dd725a6e8
kkang123 <one98kr@gmail.com>
2025-09-14 17:42:41 +0900

    refactor(profile) : 코드 재사용성 위해 컴포넌트 분리

---
+++ apps/frontend/app/_components/Menu/MobileMenu.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import { useAuthStore } from "../../_store/authStore";
+
+import ProfileButton from "../Button/ProfileButton";
+import LoginStatusButton from "../Header/LoginStatusButton";
+import MenuButton from "../Button/MenuButton";
+
+const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const user = useAuthStore((state) => state.user);
+
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+
+  if (!user) {
+    return <LoginStatusButton />;
+  }
+
+  return (
+    <div className="sm:hidden">
+      <MenuButton onClick={toggleMenu} />
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-32 bg-gray-700 rounded-md shadow-lg py-1 z-50">
+          <div className="px-4 py-2">
+            <ProfileButton />
+          </div>
+          <div className="px-4 py-2">
+            <LoginStatusButton />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MobileMenu;
