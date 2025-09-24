df1f0eb93e2957b2f4e7ac2ef55218ad93d4270e
kkang123 <one98kr@gmail.com>
2025-09-24 14:50:22 +0900

    feat : GET /problems API에 date 쿼리 파라미터 추가하여 날짜별 문제 조회 기능 지원

--- apps/backend/src/problem/controller/problem.controller.ts
+++ apps/backend/src/problem/controller/problem.controller.ts
@@ -1,30 +1,46 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Req,
+    UseGuards,
+    Query, // 추가
+} from '@nestjs/common';
 import { ApiPath } from '../../api-path';
 import { ProblemService } from '../service';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 import { User } from '../../common/decorators';
 import { SubmitProblemRequestDto } from '../dto/request/submit-problem.request';
-import { GetProblemsSwagger, ProblemControllerSwagger, SubmitProblemSwagger } from '../swagger';
+import {
+    GetProblemsSwagger,
+    ProblemControllerSwagger,
+    SubmitProblemSwagger,
+} from '../swagger';
 
 @Controller()
 @ProblemControllerSwagger
 export class ProblemController {
-  constructor(private readonly problemService: ProblemService) {
-  }
+    constructor(private readonly problemService: ProblemService) {}
 
-  @UseGuards(JwtAuthGuard)
-  @Post(ApiPath.Problem.PROBLEMS)
-  @SubmitProblemSwagger
-  async submit(
-    @User() memberId: number,
-    @Body() dto: SubmitProblemRequestDto) {
-    await this.problemService.submit(memberId, dto);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Post(ApiPath.Problem.PROBLEMS)
+    @SubmitProblemSwagger
+    async submit(
+        @User() memberId: number,
+        @Body() dto: SubmitProblemRequestDto,
+    ) {
+        await this.problemService.submit(memberId, dto);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get(ApiPath.Problem.PROBLEMS)
-  @GetProblemsSwagger
-  async getProblems(@User() memberId: number) {
-    return this.problemService.getList(memberId);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get(ApiPath.Problem.PROBLEMS)
+    @GetProblemsSwagger
+    async getProblems(@User() memberId: number, @Query('date') date?: string) {
+        // 추가
+        if (date) {
+            return this.problemService.getListByDate(memberId, date);
+        }
+        return this.problemService.getList(memberId);
+    }
 }
