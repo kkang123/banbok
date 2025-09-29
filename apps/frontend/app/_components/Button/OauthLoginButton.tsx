86599b69df8ab845ee8f814ef7b2d178df976ab6
kkang123 <one98kr@gmail.com>
2025-09-29 14:39:19 +0900

    refactor : api 하드 코딩 경로 상수화 및 사용하지 않는 구글 로그인버튼 제거

--- apps/frontend/app/_components/Button/OauthLoginButton.tsx
+++ apps/frontend/app/_components/Button/OauthLoginButton.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 
+import { API_BASE_URL, ENDPOINTS } from "@/app/_constants/api";
+
 export function NaverLogin() {
   const handleNaverLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/naver`;
+    window.location.href = `${API_BASE_URL}${ENDPOINTS.NAVER_LOGIN}`;
   };
 
   return (
@@ -18,28 +20,6 @@ export function NaverLogin() {
         height={20}
       />
       <span>네이버 로그인</span>
-    </button>
-  );
-}
-
-export function GoogleLogin() {
-  const handleGoogleLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/v1/oauth/google`;
-  };
-
-  return (
-    <button
-      onClick={handleGoogleLogin}
-      className="mt-2 flex w-full items-center justify-center gap-2 rounded-lg border-2 border-red-400 p-2 transition-colors hover:bg-red-400 hover:text-white"
-      aria-label="구글 로그인"
-    >
-      <Image
-        src="assets/icon/google_icon.svg"
-        alt="구글 아이콘"
-        width={20}
-        height={20}
-      />
-      <span>구글 로그인</span>
     </button>
   );
 }
--- apps/frontend/app/_constants/api.ts
+++ apps/frontend/app/_constants/api.ts
@@ -3,4 +3,5 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export const ENDPOINTS = {
   ME: "/v1/members/me",
   PROBLEMS: "/v1/problems",
+  NAVER_LOGIN: "/oauth2/authorization/naver",
 };
